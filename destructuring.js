9123e471e65adfea88811cfbacf3149c47245f1b
Anjalit9714 <anjalit9714@gmail.com>
2024-09-28 16:55:43 +0530

    add

---
+++ destructuring.js
@@ -0,0 +1,38 @@
+// a=5;
+// b=3;
+// [a,b]=[b,a];
+// console.log(a);
+// console.log(b);
+
+// const colors=["red","green","blue","white","black"];
+// [colors[1],colors[4]]=[colors[4],colors[1]];
+// console.log(colors);
+// const [firstcolor,secondcolor,thirdcolor,...extractvalues]=colors;
+// console.log(firstcolor);
+// console.log(secondcolor);
+// console.log(thirdcolor);
+// console.log(extractvalues);
+
+
+const person1= {
+    fName:"anu",
+    lName:"t",
+    age:24
+};
+const person2= {
+    fName:"anu",
+    lName:"t",
+    
+};
+const person3= {
+    fName:"anu",
+    lName:"t",
+    age:24
+};
+const {fName,lName,age}=person2;
+
+console.log(fName);
+console.log(lName);
+console.log(age);
+
+
